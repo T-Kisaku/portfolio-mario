e7ef1bf7b9d521cbe79dfa07badfddf41988cbdc
箕作 天志 <69094508+T-Kisaku@users.noreply.github.com>
2022-06-03 20:14:46 +0900

    Create useful function

--- src/input.ts
+++ src/input.ts
@@ -1,8 +1,8 @@
-// import { Mario } from './entities/Mario'
+import { Mario } from './entities/Mario'
 import { Takashi } from './entities/Takashi'
 import { Entity } from './entities/Entity'
 import { InputRouter } from './InputRouter'
-import { Keyboard } from './Keyboard'
+import { Keyboard, KeyListener } from './Keyboard'
 import { Go } from './traits/Go'
 import { Jump } from './traits/Jump'
 
@@ -10,12 +10,14 @@ export function setupKeyboard(target: EventTarget) {
   const input = new Keyboard()
   const router = new InputRouter<Entity>()
 
+  const addMultiEventListener = (keyList: string[], listener: KeyListener) => keyList.forEach(key => input.addListener(key, listener))
+
   let leftState = 0
   let rightState = 0
 
   input.listenTo(target)
 
-  input.addListener('ArrowRight', (keyState) => {
+  addMultiEventListener(['ArrowRight', 'KeyD'], keyState => {
     rightState = keyState
     router.route((entity) => {
       entity.useTrait(Go, (go) => {
@@ -24,7 +26,7 @@ export function setupKeyboard(target: EventTarget) {
     })
   })
 
-  input.addListener('ArrowLeft', (keyState) => {
+  addMultiEventListener(['ArrowLeft', 'KeyA'], keyState => {
     leftState = keyState
     router.route((entity) => {
       entity.useTrait(Go, (go) => {
@@ -33,30 +35,9 @@ export function setupKeyboard(target: EventTarget) {
     })
   })
 
+
   /** Jump */
-  input.addListener('KeyZ', (pressed) => {
-    if (pressed) {
-      router.route((entity) => {
-        entity.useTrait(Jump, (jump) => jump.start())
-      })
-    } else {
-      router.route((entity) => {
-        entity.useTrait(Jump, (jump) => jump.cancel())
-      })
-    }
-  })
-  input.addListener('Space', (pressed) => {
-    if (pressed) {
-      router.route((entity) => {
-        entity.useTrait(Jump, (jump) => jump.start())
-      })
-    } else {
-      router.route((entity) => {
-        entity.useTrait(Jump, (jump) => jump.cancel())
-      })
-    }
-  })
-  input.addListener('ArrowUp', (pressed) => {
+  addMultiEventListener(['KeyZ', 'Space', 'ArrowUp'], pressed => {
     if (pressed) {
       router.route((entity) => {
         entity.useTrait(Jump, (jump) => jump.start())
@@ -69,41 +50,14 @@ export function setupKeyboard(target: EventTarget) {
   })
 
   /** Speed up */
-  input.addListener('KeyX', (keyState) => {
+  addMultiEventListener(['KeyX', 'KeyF'], keyState => {
     router.route((entity) => {
       // the turbo should probably be a separate trait
-      if (entity instanceof Takashi) {
+      if (entity instanceof Takashi || entity instanceof Mario) {
         entity.setTurboState(keyState === 1)
       }
     })
   })
-  input.addListener('KeyF', (keyState) => {
-    router.route((entity) => {
-      // the turbo should probably be a separate trait
-      if (entity instanceof Takashi) {
-        entity.setTurboState(keyState === 1)
-      }
-    })
-  })
-
-    /** Speed up */
-    // input.addListener('KeyX', (keyState) => {
-    //   router.route((entity) => {
-    //     // the turbo should probably be a separate trait
-    //     if (entity instanceof Mario) {
-    //       entity.setTurboState(keyState === 1)
-    //     }
-    //   })
-    // })
-    // input.addListener('KeyF', (keyState) => {
-    //   router.route((entity) => {
-    //     // the turbo should probably be a separate trait
-    //     if (entity instanceof Mario) {
-    //       entity.setTurboState(keyState === 1)
-    //     }
-    //   })
-    // })
-  
 
   return router
 }
