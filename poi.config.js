90f291ca2e5650bfae85bb4718cf306e9ed4ed1b
箕作 天志 <69094508+T-Kisaku@users.noreply.github.com>
2022-06-08 14:19:10 +0900

    Migrate poi to vite

--- poi.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  entry: './src/main.ts',
-  publicFolder: './public',
-  output: {
-    dir: 'docs',
-    publicUrl: 'https://t-kisaku.github.io/portfolio-mario/portfolio-mario/'
-  }
-}
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('vite').UserConfig}
+ */
+ const config = {
+    publicDir: 'public',
+    build: {
+        outDir: 'docs',
+    }
+}
+
+export default config
